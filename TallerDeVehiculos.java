107dd46e357b3b9c26192ac8fc0b597769a8cfa5
TessDuarte <153113428+TessDuarte@users.noreply.github.com>
2024-10-23 14:32:36 -0600

    Add files via upload

---
+++ TallerDeVehiculos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tallerdevehiculos;
+
+/**
+ *
+ * @author Tessd
+ */
+
+public class TallerDeVehiculos {
+      public TallerDeVehiculos string nombrepersona,string nombrevehiculo) 
+      {
+          this.nombrepersona=nombrepersona;
+          this.nombrevehiculo=nombrevehiculo;
+      }
+        
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
