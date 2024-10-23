830099bbe04e6b5e44a798e911d67180cc70472b
TessDuarte <153113428+TessDuarte@users.noreply.github.com>
2024-10-23 16:47:45 -0600

    Add files via upload

---
+++ GestionTaller.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tallerdevehiculos;
+
+/**
+ *
+ * @author Tessd
+ */
+
+//Clase necesaria para crear un flujo basico de entrada de datos.
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+
+public class GestionTaller {
+
+    //Creamos un nuevo objeto Taller para guardar en las listas de clientes, vehiculos 
+    private Taller taller = new Taller();
+}
+
+     private static final String menuPrincipal[] = {
+        "0. Salir del programa",
+        "1. Gestion de clientes",
+        "2. Gestion de vehiculos",
+        "3. Estado general del taller"
+    };
+
+    private static final String menuCliente[] = {
+        "0. Volver al menu principal",
+        "1. Alta de nuevo cliente",
+        "2. Baja de cliente existente",
+        "3. Mostrar datos de un cliente",
+        "4. Mostrar datos de todos los clientes",
+        "5. Modificar datos de un cliente"
+    };
+     
+}
+
+   
